ebcf8ba1af2ce7fa1483b8979d3c5ed02613675f
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2023-02-22 08:46:26 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb://127.0.0.1:27017/WikiDB");
+
+app.set("view engine", "ejs");
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function (req, res) {
+  Article.find(function (err, foundArticle) {
+    if (!err) {
+      res.send(foundArticle);
+    } else {
+      res.send(err);
+    }
+  });
+});
+app.post("/articles", function (req, res) {
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  newArticle.save(function(err){
+    if(!err){
+        res.send("New Article Added")
+    } else {
+        res.send(err)
+    }
+  });
+});
+app.delete("/articles", function (req, res) {
+   
+    Article.deleteMany(function(err){
+        if(!err){
+            res.send("Successfully Deleted all Articles")
+        } else {
+            res.send(err)
+        }
+    })
+
+  });
+
+app.listen("7007", function () {
+  console.log("Server is running correctly");
+});
