4198530150c49b158f9377ef5fd3edd5d656f398
Are Oluwasegun <37227404+AceRex@users.noreply.github.com>
2023-02-22 12:38:00 +0100

    .Delete

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
 mongoose.set("strictQuery", true);
 mongoose.connect("mongodb://127.0.0.1:27017/WikiDB");
 
@@ -19,42 +18,86 @@ const articleSchema = new mongoose.Schema({
 
 const Article = mongoose.model("Article", articleSchema);
 
+// All Article
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, foundArticle) {
+      if (!err) {
+        res.send(foundArticle);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("New Article Added");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("Successfully Deleted all Articles");
+      } else {
+        res.send(err);
+      }
+    });
+  });
 
-app.route("/articles")
-    .get(function (req, res) {
-        Article.find(function (err, foundArticle) {
-          if (!err) {
-            res.send(foundArticle);
-          } else {
-            res.send(err);
-          }
-        });
-      })
-      .post(function (req, res) {
-        const newArticle = new Article({
-          title: req.body.title,
-          content: req.body.content,
-        });
-        newArticle.save(function(err){
-          if(!err){
-              res.send("New Article Added")
-          } else {
-              res.send(err)
-          }
-        });
-      })
-      .delete(function (req, res) {
-         
-          Article.deleteMany(function(err){
-              if(!err){
-                  res.send("Successfully Deleted all Articles")
-              } else {
-                  res.send(err)
-              }
-          })
-      
-        });
+// Specific Article
 
+app
+  .route("/articles/:articleTitle")
+  .get(function (req, res) {
+    Article.findOne(
+      { title: req.params.articleTitle },
+      function (err, foundArticle) {
+        if (foundArticle) {
+          res.send(foundArticle);
+        } else {
+          res.send("Article not on list");
+        }
+      }
+    );
+  })
+  .put(function (req, res) {
+    Article.updateOne(
+      { title: req.params.articleTitle },
+      { title: req.body.title, content: req.body.content },
+      function (err) {
+        if (!err) {
+          res.send("Successfully Updated");
+        }
+      }
+    );
+  })
+  .patch(function (req, res) {
+    Article.updateOne(
+      { title: req.params.articleTitle },
+      { $set: req.body },
+      function (err) {
+        if (!err) {
+          res.send("Successfully Updated");
+        }
+      }
+    );
+  })
+  .delete(function (req, res) {
+    Article.deleteOne({ title: req.params.articleTitle }, function (err) {
+      if (!err) {
+        res.send("Successfully deleted Article");
+      }
+    });
+  });
 
 app.listen("7007", function () {
   console.log("Server is running correctly");
